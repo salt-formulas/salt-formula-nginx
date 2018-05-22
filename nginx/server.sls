salt-formulas/salt-formula-nginx

commit 621ee472a6ea57474bb6a20241eded83c09db736

Creates /etc/ssl/dhparams.pem only when actually needed
(so that we can skip this cpu intensive operation on small devices like
raspberry pi, when secure ssl is not required)

nginx/server.sls
@@ -78,6 +78,14 @@ nginx_service:
   - require:
     - pkg: nginx_packages
 
+{%- set generate_dhparams = False %}
+{%- for site_name, site in server.get('site', {}).iteritems() %}
+{%- if site.get('ssl', {}).get('enabled') and site.ssl.get('mode', 'secure') == 'secure' %}
+  {%- set generate_dhparams = True %}
+{%- endif %}
+{%- endfor %}
+
+{%- if generate_dhparams %}
 nginx_generate_dhparams:
   cmd.run:
   - name: openssl dhparam -out /etc/ssl/dhparams.pem 2048
@@ -86,5 +94,6 @@ nginx_generate_dhparams:
     - pkg: nginx_packages
   - watch_in:
     - service: nginx_service
+{%- endif %}
 
 {%- endif %}

commit 6b43faf4d420c5081a0498bbbb32332e808d9850

server: Fix generate_dhparams j2 var manipulation
Fixes: 621ee472

Signed-off-by: Alexandru Avadanii <Alexandru.Avadanii@enea.com>

nginx/server.sls
@@ -78,14 +78,14 @@ nginx_service:
   - require:
     - pkg: nginx_packages
 
-{%- set generate_dhparams = False %}
+{%- set generate_dhparams = { 'enabled': False } %}
 {%- for site_name, site in server.get('site', {}).iteritems() %}
 {%- if site.get('ssl', {}).get('enabled') and site.ssl.get('mode', 'secure') == 'secure' %}
-  {%- set generate_dhparams = True %}
+  {%- do generate_dhparams.update({ 'enabled': True }) %}
 {%- endif %}
 {%- endfor %}
 
-{%- if generate_dhparams %}
+{%- if generate_dhparams['enabled'] %}
 nginx_generate_dhparams:
   cmd.run:
   - name: openssl dhparam -out /etc/ssl/dhparams.pem 2048